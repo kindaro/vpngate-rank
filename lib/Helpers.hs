9b99576afef4bdb1eb1a0fe2c6cb8de0a42047fd
Ignat Insarov <kindaro@gmail.com>
2019-08-07 23:15:58 +0300

    Lay cornerstone of Helpers.

---
+++ lib/Helpers.hs
@@ -0,0 +1,37 @@
+module Helpers where
+
+import Prelude
+import Control.Exception
+
+
+-- I will maybe call this module "sequence" or something like that.
+
+-- Run all of the action and collect all the results and synchronous exceptions.
+--
+-- An asynchronous exception must lead to immediate termination.
+--
+independent :: (Monad (m e), Foldable f, Monoid (q e), Monoid (q a))
+            => f (m e a) -> m (q e) (q a)
+
+independent actions = undefined
+
+insistent :: (Monad (m e), Monoid (q e))
+          => Word -> m e a -> m (q e) (Maybe a)
+insistent = undefined
+
+redundant :: (Monad (m e), Foldable f, Monoid (q e))
+          => f (m e a) -> m (q e) a
+redundant = undefined
+
+-- Log everything going in and out, and provide the same for analysis.
+--
+-- Logging is a monadic action, so loggers can be sewn inside the environment.
+--
+-- We must edit the process description to launch the `timeout` program available on Unix.
+--
+-- transparent :: ProcessDescription a b c -> 
+
+data Exceptions = forall e. Exception e => Exceptions e
+
+handleSynchronous :: Exception e => (e -> IO a) -> IO a -> IO (Either e a)
+handleSynchronous = undefined
